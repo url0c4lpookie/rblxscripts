148ef50f82fe6e68f84235bacb38e01c7f31cfd6
url0c4lpookie <keeper.seater.07@gmail.com>
2024-08-31 16:34:49 -0500

    Update lagswitch.lua

--- lagswitch.lua
+++ lagswitch.lua
@@ -1,1 +1,2 @@
+-- Click X to Activate/Deactivate Lag Switch.
 loadstring(game:HttpGet("https://paste.gg/p/anonymous/d3492bd620bd4c46b54680ac80b0fa0d/files/161ddd9c992c4ceba3c298458d45188a/raw", true))()
