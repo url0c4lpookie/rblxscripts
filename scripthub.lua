c46d85393ae96bcd4d846fea839bd7343cb6ba73
urlocalpookie <keeper.seater.07@gmail.com>
2024-09-01 15:16:02 -0500

    Create scripthub.lua

---
+++ scripthub.lua
@@ -0,0 +1,21 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "lmachild2011's Script Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Noclip",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "Noclip"
+})
+Tab:AddButton({
+	Name = "Noclip",
+	Callback = function()
+	    for i, part in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
+	        if part:IsA("BasePart") or part:IsA("MeshPart") then
+	            part.CanCollide = false
+	        end
+	    end
+  	end    
+})
