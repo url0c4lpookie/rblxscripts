3414e9f3919b386bd70e466031fd13068ae5a80c
urlocalpookie <keeper.seater.07@gmail.com>
2024-09-01 18:43:24 -0500

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -26,6 +26,32 @@ Tab:AddBind({
 	    end
 	end    
 })
+Tab:AddButton({
+	Name = "First Person Body",
+	Callback = function()
+ --//Variables\\--
+local player =  game:GetService("Players").LocalPlayer
+local character = player.character or player.characterAdded:Wait()
+
+--//Events\\--
+game:GetService("RunService").RenderStepped:Connect(function()
+ --//Visible\\--
+ for i, part in pairs(character:GetChildren())do
+  if string.match(part.Name, "Arm") or string.match(part.Name, "Hand") or string.match(part.Name, "Torso") or string.match(part.Name, "Leg") then
+   part.LocalTransparencyModifier = 0
+  end
+ end
+end)
+  	end
+})
+Tab:AddTextbox({
+	Name = "Camera Offset",
+	Default = "0,0,0",
+	TextDisappear = true,
+	Callback = function(Value)
+game.Players.LocalPlayer.Character.Humanoid.CameraOffset = Vector3.new(Value)
+	end	  
+})
 local Tab = Window:MakeTab({
 	Name = "Lag Switch",
 	Icon = "rbxassetid://0",
